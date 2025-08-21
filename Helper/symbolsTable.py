d883811d10440d428c18ccc199d0346de130f737
Rugroso <133253941+Rugroso@users.noreply.github.com>
2025-08-21 16:09:21 -0700

    more symbols

--- Helper/symbolsTable.py
+++ Helper/symbolsTable.py
@@ -1,7 +1,71 @@
 tokens = {
+    "MAIN": {
+        "regex": r"fish",
+        "example": "fish"
+    },
+    "IMPORTAR": {
+        "regex": r"fishport",
+        "example": "fishport"
+    },
     "IF": {
-        "regex": r"if",
-        "example": "if"
+        "regex": r"<if",
+        "example": "<if"
+    },
+    "ELSE": {
+        "regex": r"<else",
+        "example": "<else"
+    },
+    "FUNCION": {
+        "regex": r"fishtion",
+        "example": "fishtion"
+    },
+    "TRY": {
+        "regex": r"<try",
+        "example": "<try"
+    },
+    "CATCH": {
+        "regex": r"<catch",
+        "example": "<catch"
+    },
+    "FINALLY": {
+        "regex": r"<finally",
+        "example": "<finally"
+    },
+    "RETURN": {
+        "regex": r"emerge",
+        "example": "emerge"
+    },
+    "ENTERO": {
+        "regex": r"<int",
+        "example": "<int"
+    },
+    "STRING": {
+        "regex": r"<string",
+        "example": "<string"
+    },
+    "CHAR": {
+        "regex": r"<char",
+        "example": "<char"
+    },
+    "FLOAT": {
+        "regex": r"<bubble",
+        "example": "<bubble"
+    },
+    "BOOLEANO": {
+        "regex": r"<hook",
+        "example": "<hook"
+    },
+    "WHILE": {
+        "regex": r"whale",
+        "example": "whale"
+    },
+    "FOR": {
+        "regex": r"forK",
+        "example": "fork"
+    },
+    "PRINT": {
+        "regex": r"splash",
+        "example": "splash"
     },
     "ID": {
         "regex": r"[a-zA-Z_][a-zA-Z0-9_]*",
@@ -12,15 +76,39 @@ tokens = {
         "example": "42, 3.14"
     },
     "ASIGNACION": {
-        "regex": r"=",
-        "example": "="
+        "regex": r"<=",
+        "example": "<="
     },
     "OP_SUMA": {
-        "regex": r"\+",
-        "example": "+"
+        "regex": r"\<+",
+        "example": "<+"
+    },
+    "OP_RESTA": {
+        "regex": r"<-",
+        "example": "<-"
+    },
+    "OP_MULTIPLICACION": {
+        "regex": r"<*",
+        "example": "<*"
+    },
+    "OP_DIVISION": {
+        "regex": r"</",
+        "example": "</"
+    },
+    "OP_MODULO": {
+        "regex": r"<%",
+        "example": "<%"
+    },
+    "OP_MENOR_QUE": {
+        "regex": r"<<",
+        "example": "<<"
+    },
+    "OP_MAYOR_QUE": {
+        "regex": r"<<>",
+        "example": "<<>"
     },
     "DELIMITADOR": {
-        "regex": r";",
-        "example": ";"
+        "regex": r"<\D",
+        "example": "<D"
     }
 }
