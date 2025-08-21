f9c345fae83203111556c0c1b822d24f74a01af3
Rugroso <133253941+Rugroso@users.noreply.github.com>
2025-08-21 15:37:09 -0700

    dummysymbols

---
+++ Helper/symbolsTable.py
@@ -0,0 +1,26 @@
+tokens = {
+    "IF": {
+        "regex": r"if",
+        "example": "if"
+    },
+    "ID": {
+        "regex": r"[a-zA-Z_][a-zA-Z0-9_]*",
+        "example": "contador"
+    },
+    "NUM": {
+        "regex": r"[0-9]+(\.[0-9]+)?",
+        "example": "42, 3.14"
+    },
+    "ASIGNACION": {
+        "regex": r"=",
+        "example": "="
+    },
+    "OP_SUMA": {
+        "regex": r"\+",
+        "example": "+"
+    },
+    "DELIMITADOR": {
+        "regex": r";",
+        "example": ";"
+    }
+}
